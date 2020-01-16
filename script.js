572855d085f4d2e4568b87c6d11126d3502046a7
Umrbek <Umrbek@bk.ru>
2020-01-16 21:23:10 +0500

    js1

---
+++ script.js
@@ -0,0 +1,56 @@
+
+/* 
+console.log()- метод вывода информации в консол в браузера для разработчика
+*/
+
+console.log(1);         /* Числовой тип данных */
+console.log('Umrbek'); /* строковый тип данных.Обезателно должни быт в кавычках */
+console.log(true);    /* Boolean--логическое тип данных (true-истина False-лож)  */
+
+var num = 10;
+/* var ключовая слова для создания переменной */
+console.log(num);
+var Num=11;
+console.log(Num);
+
+
+
+var und;
+console.log(und);/* underfined - не определено */
+
+var name="bek";
+var year=50;
+var str = " Я " + name + " мне "+ year + " лет! "
+console.log(str); /* (+) - склеевна строк  */
+// конкатенация
+
+console.log(6+3);
+console.log(6-3);
+console.log(6*3);
+console.log(6/3);
+console.log(16 % 3);/*  % деленеий с остатком например16 %3 =16-15=1 */
+
+
+console.log(" 6 + 3= " +(6+3));
+console.log(" 6-3=" + (6 - 3) );
+console.log("6*3=" +6*3);
+console.log("6/3 = "+ 6/3);
+console.log("16 % 3 = " + 16%3);
+
+var name1 = prompt("Введите своё имя");
+/* prompt---функция сообщение с способом ввода данных */
+console.log(name1);
+
+var age= +prompt( " Ваш возраст");
+
+alert(age);
+/* alert- - спливаюшие окна */
+
+
+
+
+
+
+
+
+
